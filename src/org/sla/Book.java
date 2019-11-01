643ffef024a7fd7ed95267f4cc95737076005d0c
jackmotter <jackmotter3@gmail.com>
2019-11-01 13:19:12 -0400

    added getters and setters

--- src/org/sla/Book.java
+++ src/org/sla/Book.java
@@ -10,10 +10,60 @@ public class Book {
     private String genre;
 
     public Book(String title, String author, int sales, int publish, String language, String genre){
-
+        this.author = author;
+        this.genre = genre;
+        this.language = language;
+        this.publish = publish;
+        this.sales = sales;
+        this.title = title;
 
     }
 
+    public String getTitle() {
+        return title;
+    }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getSales() {
+        return sales;
+    }
+
+    public void setSales(int sales) {
+        this.sales = sales;
+    }
+
+    public int getPublish() {
+        return publish;
+    }
+
+    public void setPublish(int publish) {
+        this.publish = publish;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
