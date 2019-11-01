8265c825901602b630f7461c5f967d4fdf11a787
HaydenCM <hmyers21@scienceleadership.org>
2019-11-01 13:45:41 -0400

    finished manga

--- src/org/sla/Main.java
+++ src/org/sla/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        Manga manga1 = new Manga("One Piece", "Eiichiro Oda", 455000000, 1997, "japanese", "manga", "Shueisha", "Shonen", 94);
+         System.out.println(manga1.getLanguage());
+
 	// write your code here
     }
 }
