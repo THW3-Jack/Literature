4a5dd4d556fa150a755f4c3850035a2d32ed4412
jackmotter <jackmotter3@gmail.com>
2019-11-01 13:01:09 -0400

    added fields

--- src/org/sla/Book.java
+++ src/org/sla/Book.java
@@ -1,4 +1,12 @@
 package org.sla;
 
 public class Book {
+
+    private String title;
+    private String author;
+    private int sales;
+    private int publish;
+    private String language;
+    private String genre;
+
 }
